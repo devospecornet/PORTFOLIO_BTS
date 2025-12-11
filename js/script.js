8fb2c4560fd701ac0f6e238c345335f365fbe3fa
erica <erica.cornet.dev@gmail.com>
2025-12-11 17:53:34 +0100

    initialisation commit

---
+++ js/script.js
@@ -0,0 +1,74 @@
+const photo1 = document.getElementById("imgcl");
+photo1.classList.add("img1");
+//
+let clic1 = document.getElementById("clic1");
+let afficheContenu1 = document.getElementById("imageVisible1");
+let croix1 = document.getElementById("croix1");
+
+clic1.addEventListener("click", function () {
+  afficheContenu1.classList.add("afficher");
+});
+
+croix1.addEventListener("click", function () {
+  afficheContenu1.classList.remove("afficher");
+});
+
+let clic2 = document.getElementById("clic2");
+let afficheContenu2 = document.getElementById("imageVisible2");
+let croix2 = document.getElementById("croix2");
+
+clic2.addEventListener("click", function () {
+  afficheContenu2.classList.add("afficher");
+});
+
+croix2.addEventListener("click", function () {
+  afficheContenu2.classList.remove("afficher");
+});
+
+let clic3 = document.getElementById("clic3");
+let afficheContenu3 = document.getElementById("imageVisible3");
+let croix3 = document.getElementById("croix3");
+
+clic3.addEventListener("click", function () {
+  afficheContenu3.classList.add("afficher");
+});
+
+croix3.addEventListener("click", function () {
+  afficheContenu3.classList.remove("afficher");
+});
+
+let clic4 = document.getElementById("clic4");
+let afficheContenu4 = document.getElementById("imageVisible4");
+let croix4 = document.getElementById("croix4");
+
+clic4.addEventListener("click", function () {
+  afficheContenu4.classList.add("afficher");
+});
+
+croix4.addEventListener("click", function () {
+  afficheContenu4.classList.remove("afficher");
+});
+
+let clic5 = document.getElementById("clic5");
+let afficheContenu5 = document.getElementById("imageVisible5");
+let croix5 = document.getElementById("croix5");
+
+clic5.addEventListener("click", function () {
+  afficheContenu5.classList.add("afficher");
+});
+
+croix5.addEventListener("click", function () {
+  afficheContenu5.classList.remove("afficher");
+});
+
+let clic6 = document.getElementById("clic6");
+let afficheContenu6 = document.getElementById("imageVisible6");
+let croix6 = document.getElementById("croix6");
+
+clic6.addEventListener("click", function () {
+  afficheContenu6.classList.add("afficher");
+});
+
+croix6.addEventListener("click", function () {
+  afficheContenu6.classList.remove("afficher");
+});
