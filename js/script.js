f49026087690b7dc090d489277aa66da63a6a756
erica <erica.cornet.dev@gmail.com>
2026-01-09 13:54:16 +0100

    Initial commit - portfolio

--- js/script.js
+++ js/script.js
@@ -7,10 +7,12 @@ let croix1 = document.getElementById("croix1");
 
 clic1.addEventListener("click", function () {
   afficheContenu1.classList.add("afficher");
+  document.body.classList.add("modal-open");
 });
 
 croix1.addEventListener("click", function () {
   afficheContenu1.classList.remove("afficher");
+  document.body.classList.remove("modal-open");
 });
 
 let clic2 = document.getElementById("clic2");
@@ -19,10 +21,12 @@ let croix2 = document.getElementById("croix2");
 
 clic2.addEventListener("click", function () {
   afficheContenu2.classList.add("afficher");
+  document.body.classList.add("modal-open");
 });
 
 croix2.addEventListener("click", function () {
   afficheContenu2.classList.remove("afficher");
+  document.body.classList.remove("modal-open");
 });
 
 let clic3 = document.getElementById("clic3");
@@ -31,10 +35,12 @@ let croix3 = document.getElementById("croix3");
 
 clic3.addEventListener("click", function () {
   afficheContenu3.classList.add("afficher");
+  document.body.classList.add("modal-open");
 });
 
 croix3.addEventListener("click", function () {
   afficheContenu3.classList.remove("afficher");
+  document.body.classList.remove("modal-open");
 });
 
 let clic4 = document.getElementById("clic4");
@@ -43,10 +49,12 @@ let croix4 = document.getElementById("croix4");
 
 clic4.addEventListener("click", function () {
   afficheContenu4.classList.add("afficher");
+  document.body.classList.add("modal-open");
 });
 
 croix4.addEventListener("click", function () {
   afficheContenu4.classList.remove("afficher");
+  document.body.classList.remove("modal-open");
 });
 
 let clic5 = document.getElementById("clic5");
@@ -55,10 +63,12 @@ let croix5 = document.getElementById("croix5");
 
 clic5.addEventListener("click", function () {
   afficheContenu5.classList.add("afficher");
+  document.body.classList.add("modal-open");
 });
 
 croix5.addEventListener("click", function () {
   afficheContenu5.classList.remove("afficher");
+  document.body.classList.remove("modal-open");
 });
 
 let clic6 = document.getElementById("clic6");
@@ -67,8 +77,10 @@ let croix6 = document.getElementById("croix6");
 
 clic6.addEventListener("click", function () {
   afficheContenu6.classList.add("afficher");
+  document.body.classList.add("modal-open");
 });
 
 croix6.addEventListener("click", function () {
   afficheContenu6.classList.remove("afficher");
+  document.body.classList.remove("modal-open");
 });
